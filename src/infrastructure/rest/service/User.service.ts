7ac02368a24983da324b43d1edaa29a7d114f46e
Abdias Montalvão <abdiasjunnior@outlook.com>
2021-09-28 16:56:39 -0300

    fix(turn into a singleton)

--- src/infrastructure/rest/service/User.service.ts
+++ src/infrastructure/rest/service/User.service.ts
@@ -1,19 +1,38 @@
 import { GetUsers } from "../../../application/usecase/GetUsers";
+import { CreateUser } from "../../../application/usecase/CreateUser";
+import { UpdateUser } from "../../../application/usecase/UpdateUser";
 import { BasicUserInfoDTO } from "../../../domain/dto/BasicUserInfoDTO";
-import { UserRepositoryInMemory } from "../../repository/UserRepositoryInMemory";
+import userRepository from "../../repository/UserRepositoryInMemory";
+import { CreateUserDTO } from "../../../domain/dto/CreateUserDTO";
+import { UpdateUserDTO } from "../../../domain/dto/UpdateUserDTO";
 
 export class UserService {
 
   private readonly getUsers: GetUsers;
+  private readonly createUser: CreateUser;
+  private readonly updateUser: UpdateUser;
 
   constructor() {
-    const userRepository = new UserRepositoryInMemory();
     this.getUsers = new GetUsers(userRepository);
+    this.createUser = new CreateUser(userRepository);
+    this.updateUser = new UpdateUser(userRepository);
   }
 
   public async getAll(): Promise<Array<BasicUserInfoDTO>> {
     const users = await this.getUsers.execute();
 
     return users;
+  }
+
+  public async create(userData: CreateUserDTO): Promise<BasicUserInfoDTO> {
+    const user = await this.createUser.execute(userData);
+
+    return user;
+  }
+
+  public async save(userData: UpdateUserDTO): Promise<BasicUserInfoDTO> {
+    const user = await this.updateUser.execute(userData);
+
+    return user;
   }
 }
